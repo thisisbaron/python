deea377cfb7c1718dc1bcded99bd923bcbd6a404
thisisbaron <thisisbaronsun@.com>
2025-04-08 17:28:25 +0800

    post成功后的修改

--- 接口.py
+++ 接口.py
@@ -1,3 +1,4 @@
+from anaconda_project.internal.conda_api import result
 from flask import Flask,request,jsonify
 #jsonify作用转换json字符串
 app = Flask(__name__)
@@ -9,7 +10,7 @@ def get():
 #http://127.0.0.1:9000/get?number=123&word=hello
 
 
-@app.route('/post', methods=['POST'])#falsk应用在执行要用post方法才能在postman中运行出来
+@app.route('/post', methods=['POST'])#falsk应用在执行要用post方法才能
 def post():
     json_data = request.get_json()
     number = json_data['number']
@@ -17,7 +18,13 @@ def post():
     list = json_data['list']
     dict = json_data['dict']
     max_number=max(list)
-    return jsonify({'number':number,'word':word,'list':list,'dict':dict})
+    result = jsonify(
+        {'number':number,
+         'word':word,
+         'list':list,
+         'dict':dict}
+    )
+    return result
 
 
 if __name__ == '__main__':
